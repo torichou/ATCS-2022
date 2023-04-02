328492d2ddfb43b2d4335aa1df4a7acc286a0f6d
torichou <tori.chou@menloschool.org>
2023-04-02 13:06:10 -0700

    login, logout, startup, run, follow, tweet

---
+++ twitter2/models.py
@@ -0,0 +1,97 @@
+"""
+The file that holds the schema/classes
+that will be used to create objects
+and connect to data tables.
+"""
+
+from sqlalchemy import ForeignKey, Column, INTEGER, TEXT, DATETIME
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Columns
+    username = Column("username", TEXT, primary_key=True)
+    password = Column("password", TEXT, nullable=False)
+    
+    following = relationship("User", 
+                             secondary="followers",
+                             primaryjoin="User.username==Follower.follower_id",
+                             secondaryjoin="User.username==Follower.following_id")
+    
+    followers = relationship("User", 
+                             secondary="followers",
+                             primaryjoin="User.username==Follower.following_id",
+                             secondaryjoin="User.username==Follower.follower_id",
+                             overlaps="following")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+
+    def __repr__(self):
+        return "@" + self.username
+
+class Follower(Base):
+    __tablename__ = "followers"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    follower_id = Column('follower_id', TEXT, ForeignKey('users.username'))
+    following_id = Column('following_id', TEXT, ForeignKey('users.username'))
+
+    def __init__(self, follower_id, following_id):
+        self.follower_id = follower_id
+        self.following_id = following_id
+
+class Tweet(Base):
+    # TODO: Complete the class
+    __tablename__ = "tweets"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", TEXT, nullable=False)
+    timestamp = Column("timestamp", TEXT, nullable=False)
+    username = Column("username", ForeignKey("users.username"))
+    tags = relationship("Tag", secondary="tweettags", back_populates="tweets")
+
+    def __init__(self, content, timestamp, username):
+        self.content = content
+        self.timestamp = timestamp
+        self.username = username
+
+    def __repr__(self):
+        string_tags = ""
+        for tag in str(self.tags):
+            string_tags += tag
+        return "@" + self.username + ":\n" + self.content + "\n" + string_tags + "\n" + self.timestamp
+
+class Tag(Base):
+    # TODO: Complete the class
+    __tablename__ = "tags"
+
+    # Columns 
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", TEXT, nullable=False)
+    tweets = relationship("Tweet", secondary="tweettags", back_populates="tags")
+
+    def __init__(self, content):
+        self.content = content
+        
+    def __repr__(self):
+        return self.content
+
+class TweetTag(Base):
+    # TODO: Complete the class
+    __tablename__ = "tweettags"
+
+    # Columns
+    #id = Column("id", INTEGER, primary_key=True)
+    tag_id = Column("tag_id", ForeignKey("tags.id"), primary_key=True)
+    tweet_id = Column("tweet_id", ForeignKey("tweets.id"), primary_key=True)
+
+    def __init__(self, tweet_id, tag_id):
+        self.tweet_id = tweet_id
+        self.tag_id = tag_id
---
+++ twitter2/twitter.py
@@ -0,0 +1,180 @@
+from models import *
+from database import init_db, db_session
+from datetime import datetime
+
+class Twitter:
+    current_user = None
+    """
+    The menu to print once a user has logged in
+    """
+    def print_menu(self):
+        print("\nPlease select a menu option:")
+        print("1. View Feed")
+        print("2. View My Tweets")
+        print("3. Search by Tag")
+        print("4. Search by User")
+        print("5. Tweet")
+        print("6. Follow")
+        print("7. Unfollow")
+        print("0. Logout")
+    
+    """
+    Prints the provided list of tweets.
+    """
+    def print_tweets(self, tweets):
+        for tweet in tweets:
+            print("==============================")
+            print(tweet)
+            print("==============================")
+
+    """
+    Should be run at the end of the program
+    """
+    def end(self):
+        print("Thanks for visiting!")
+        db_session.remove()
+    
+    """
+    Registers a new user. The user
+    is guaranteed to be logged in after this function.
+    """
+    def register_user(self):
+        while True:
+            username = input("What will your twitter handle be?\n")
+            pw_1 = input("Enter a password:\n")
+            pw_2 = input("Re-enter your password:\n")
+            if db_session.query(User).where(User.username==username).first() is not None:
+                print("That username is already taken.")
+            elif pw_1 != pw_2:   
+                print("The passwords don't match. Try again.")
+            else:
+                break
+        print("Welcome " + username + "!")
+        new_user = User(username, pw_1)
+        self.current_user = new_user
+        db_session.add(new_user)
+        db_session.commit()
+
+    """
+    Logs the user in. The user
+    is guaranteed to be logged in after this function.
+    """
+    def login(self):
+        while True:
+            username = input("Username: ")
+            username_user = db_session.query(User).where(User.username==username).first()
+            password = input("Password: ")
+            pw_user = db_session.query(User).where(User.password==password).first()
+            if (username_user is None or pw_user is None or username_user != pw_user):
+                print("Invalid username or password.")
+            else:
+                self.current_user = username_user
+                print("Welcome " + username)
+                break
+    
+    def logout(self):
+        self.current_user = None
+
+    """
+    Allows the user to login,  
+    register, or exit.
+    """
+    def startup(self):
+        print("\nPlease select a menu option:")
+        print("1. Login")
+        print("2. Register User")
+        print("3. Exit")
+        option = int(input(""))
+        if option == 1:
+            self.login()
+        elif option == 2:
+            self.register_user()
+        elif option == 3:
+            self.end()
+
+    def follow(self):
+        person = input("Who would you like to follow? ")
+        follow = db_session.query(User).where(User.username == person).first()
+        if follow in self.current_user.following:
+            print("You already follow " + str(follow))
+        else:
+            new_row = Follower(self.current_user.username, follow.username)
+            db_session.add(new_row)
+            db_session.commit()
+            print("You are now following " + str(follow))
+
+    def unfollow(self):
+        person = input("Who would you like to unfollow? ")
+        unfollow = db_session.query(User).where(User.username == person).first()
+        if unfollow not in self.current_user.following:
+            print("You don't follow " + str(unfollow))
+        else:
+            delete_row = db_session.query(Follower).where(Follower.follower_id==self.current_user.username and Follower.following_id==unfollow.username).first()
+            db_session.delete(delete_row)
+            db_session.commit()
+            print("You no longer follow " + str(unfollow))
+
+    def tweet(self):
+        txt = input("Create tweet: ")
+        tgs = input("Enter your tags separated by spaces: ")
+        tweet = Tweet(txt, datetime.now(), self.current_user.username)
+        db_session.add(tweet)
+        tags = tgs.split()
+        for tag in tags:
+            db_tag = db_session.query(Tag).where(Tag.content==tag).first()
+            if db_tag is None:
+                new_tag = Tag(tag)
+                db_session.add(new_tag)
+                db_session.commit()
+                db_session.add(TweetTag(tweet.id, new_tag.id))
+                db_session.commit()
+        db_session.commit()
+
+    def view_my_tweets(self):
+        lst = db_session.query(Tweet).where(Tweet.username==self.current_user.username)
+        self.print_tweets(lst)
+    
+    """
+    Prints the 5 most recent tweets of the 
+    people the user follows
+    """
+    def view_feed(self):
+        pass
+
+    def search_by_user(self):
+        pass
+
+    def search_by_tag(self):
+        pass
+
+    """
+    Allows the user to select from the 
+    ATCS Twitter Menu
+    """
+    def run(self):
+        init_db()
+
+        print("Welcome to ATCS Twitter!")
+        self.startup()
+        while True:
+            self.print_menu()
+            option = int(input(""))
+            if option == 1:
+                self.view_feed()
+            elif option == 2:
+                self.view_my_tweets()
+            elif option == 3:
+                self.search_by_tag()
+            elif option == 4:
+                self.search_by_user()
+            elif option == 5:
+                self.tweet()
+            elif option == 6:
+                self.follow()
+            elif option == 7:
+                self.unfollow()
+            else:
+                self.logout()
+                break
+        
+        self.end()
