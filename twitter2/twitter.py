36535284e160ecfca3e311bba9e1ea5dfb7cb402
torichou <tori.chou@menloschool.org>
2023-04-02 20:24:03 -0700

    finished searches

--- twitter2/twitter.py
+++ twitter2/twitter.py
@@ -106,11 +106,11 @@ class Twitter:
 
     def unfollow(self):
         person = input("Who would you like to unfollow? ")
-        unfollow = db_session.query(User).where(User.username == person).first()
+        unfollow = db_session.query(User).where(User.username==person).first()
         if unfollow not in self.current_user.following:
             print("You don't follow " + str(unfollow))
         else:
-            delete_row = db_session.query(Follower).where(Follower.follower_id==self.current_user.username and Follower.following_id==unfollow.username).first()
+            delete_row = db_session.query(Follower).where((Follower.follower_id==self.current_user.username) & (Follower.following_id==unfollow.username)).first()
             db_session.delete(delete_row)
             db_session.commit()
             print("You no longer follow " + str(unfollow))
@@ -120,6 +120,7 @@ class Twitter:
         tgs = input("Enter your tags separated by spaces: ")
         tweet = Tweet(txt, datetime.now(), self.current_user.username)
         db_session.add(tweet)
+        db_session.commit()
         tags = tgs.split()
         for tag in tags:
             db_tag = db_session.query(Tag).where(Tag.content==tag).first()
@@ -129,7 +130,9 @@ class Twitter:
                 db_session.commit()
                 db_session.add(TweetTag(tweet.id, new_tag.id))
                 db_session.commit()
-        db_session.commit()
+            else:
+                db_session.add(TweetTag(tweet.id, db_tag.id))
+                db_session.commit()
 
     def view_my_tweets(self):
         lst = db_session.query(Tweet).where(Tweet.username==self.current_user.username)
@@ -145,10 +148,24 @@ class Twitter:
             self.print_tweets(db_session.query(Tweet).where(person.following_id==Tweet.username).order_by(desc(Tweet.timestamp)).limit(5))
 
     def search_by_user(self):
-        pass
+        user = input("Search user: ")
+        person = db_session.query(User).where(user==User.username).first()
+        if person is not None:
+            self.print_tweets(db_session.query(Tweet).where(Tweet.username==person.username))
+        else:
+            print("There is no user by that name.")
 
     def search_by_tag(self):
-        pass
+        inp = input("Search tag: ")
+        tag = db_session.query(Tag).where(inp==Tag.content).first()
+        if tag is not None:
+            tweettags = db_session.query(TweetTag).where(TweetTag.tag_id==tag.id).all()
+            for tweettag in tweettags:
+                tweet = db_session.query(Tweet).where(tweettag.tweet_id==Tweet.id).all()
+                self.print_tweets(tweet)
+        else:
+            print("There are no tweets with that tag.")
+
 
     """
     Allows the user to select from the 
